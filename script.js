70c837c56c4a2af002513d76318dc3228df531e0
Mayoka Denis Kitto <92338453+yokasdenis@users.noreply.github.com>
2023-09-19 14:01:32 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+
 function firstNonRepeatedChar(str) {
- let obj={};
-	for (let i = 0; i < str.length; i++) {
-		if(obj[str[i]]!==undefined){
-			obj[str[i]]+=1;
-		}else{
-			obj[str[i]]=1;
-		}
-	}
-	for (let i = 0; i < str.length; i++) {
-		if(obj[str[i]]==1]){
-			return str[i];
-		}
-	}
-	return null;
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+    let obj={};
+       for (let i = 0; i < str.length; i++) {
+           if(obj[str[i]]!==undefined){
+               obj[str[i]]+=1;
+           }else{
+               obj[str[i]]=1;
+           }
+       }
+       for (let i = 0; i < str.length; i++) {
+           if(obj[str[i]]==1){
+               return str[i];
+           }
+       }
+       return null;
+   }
+   const input = prompt("Enter a string");
+   alert(firstNonRepeatedChar(input)); 
+   
